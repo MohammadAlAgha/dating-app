b9be01a6aed192cdd5dcab822805db90ff7236bb
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-17 21:43:39 +0200

    Added the blocks,favs and the messages tables

---
+++ backend/database/migrations/2023_03_17_193642_create_favs_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favs', function (Blueprint $table) {
+            $table->foreignId('sender_id')->constrained('id')->on('users');
+            $table->foreignId('fav_id')->constrained('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favs');
+    }
+};
---
+++ backend/database/migrations/2023_03_17_194240_create_messages_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->foreignId('sender_id')->constrained('id')->on('users');
+            $table->foreignId('receiver_id')->constrained('id')->on('users');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
