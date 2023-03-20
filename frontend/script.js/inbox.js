cf83a14fbb269476635a2dae788bcfb951f39dbf
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-20 17:14:45 +0200

    Added the incoming messages from other users

--- frontend/script.js/inbox.js
+++ frontend/script.js/inbox.js
@@ -2,6 +2,7 @@ const back = document.getElementById("back");
 const text = document.getElementById("message");
 const send = document.getElementById("send");
 const dm = document.getElementById("dm");
+const receiver = document.getElementById("receiver");
 
 back.addEventListener("click", () => {
   window.location.href = "./message.html";
@@ -11,6 +12,37 @@ const sender = JSON.parse(localStorage.getItem("User Now"));
 const receiver_id = JSON.parse(localStorage.getItem("meessagedID"));
 const sender_id = sender.id;
 
+axios.get(`http://localhost:8000/api/${receiver_id}`).then((res) => {
+  userName = res.data.user.name;
+  receiver.innerHTML = userName;
+});
+
+const body = new FormData();
+body.append("sender_id", sender_id);
+body.append("receiver_id", receiver_id);
+axios.post("http://localhost:8000/api/chat", body).then((res) => {
+  const toPrint = res.data.Message;
+  toPrint.forEach((line) => {
+    console.log(line);
+    dm.innerHTML += ` <div class="rowTo">
+    <div class="to">${line.content}</div>
+  </div>`;
+  });
+});
+
+const body2 = new FormData();
+body2.append("sender_id", receiver_id);
+body2.append("receiver_id", sender_id);
+axios.post("http://localhost:8000/api/chat", body2).then((res) => {
+  const toPrint = res.data.Message;
+  toPrint.forEach((line) => {
+    console.log(line);
+    dm.innerHTML += ` <div class="rowFrom">
+    <div class="from">${line.content}</div>
+  </div>`;
+  });
+});
+
 send.addEventListener("click", () => {
   const body = new FormData();
   body.append("sender_id", sender_id);
@@ -18,7 +50,7 @@ send.addEventListener("click", () => {
   body.append("content", text.value);
 
   axios.post("http://localhost:8000/api/messages", body).then((res) => {
-    toPrint = res.data.Message;
+    const toPrint = res.data.Message;
     dm.innerHTML += ` <div class="rowTo">
     <div class="to">${toPrint.content}</div>
   </div>`;
